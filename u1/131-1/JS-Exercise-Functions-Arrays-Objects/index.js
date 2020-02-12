c0fb6dfbc2c261aac7e7e41fd45ad40fa06e14df
Reanna <reannalacasse@gmail.com>
2020-02-11 17:28:19 -0800

    11/24

--- u1/131-1/JS-Exercise-Functions-Arrays-Objects/index.js
+++ u1/131-1/JS-Exercise-Functions-Arrays-Objects/index.js
@@ -189,9 +189,9 @@ function getCarInfoById(inventory, id) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
-}
+function sortCarInventory(inventory) {
+  return inventory.sort((a,b) => a.car_model > b.car_model ? 1 : -1);
+} // Only vaguely understand why this works, needs more study.
 
 /**
  * ### Challenge `getModelYears`
