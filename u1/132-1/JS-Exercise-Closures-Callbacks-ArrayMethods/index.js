bc34bc2dc643c3672712da76afb961766296dcda
Reanna <reannalacasse@gmail.com>
2020-02-13 21:23:14 -0800

    5 passing

--- u1/132-1/JS-Exercise-Closures-Callbacks-ArrayMethods/index.js
+++ u1/132-1/JS-Exercise-Closures-Callbacks-ArrayMethods/index.js
@@ -87,8 +87,9 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  let nl = numberList.reduce((a, b) => a + b, 0);
+  return callback(nl);
 }
 
 /**
@@ -109,8 +110,9 @@ function processSum(/* CODE HERE */) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
 */
-function processProduct(/* CODE HERE */) {
-  /* CODE HERE */
+function processProduct(num1, num2, callback) {
+  const product = num1 * num2;
+  return callback(product);
 }
 
 /**
