f710c7349e3e3c39cd1ad5dcc3350e0adba750a6
Reanna <reannalacasse@gmail.com>
2020-02-13 22:26:26 -0800

    9 passed

--- u1/132-1/JS-Exercise-Closures-Callbacks-ArrayMethods/index.js
+++ u1/132-1/JS-Exercise-Closures-Callbacks-ArrayMethods/index.js
@@ -181,8 +181,9 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  const runnerNames = runners.map(runners => `${runners.last_name}, ${runners.first_name}`);
+  return runnerNames;
 }
 
 /**
