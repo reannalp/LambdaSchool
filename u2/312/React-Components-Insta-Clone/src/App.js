8ab78b22a54296de9a2262c7d885acb346631873
Reanna <reannalacasse@gmail.com>
2020-04-14 15:19:18 -0700

    initial upload

---
+++ u2/312/React-Components-Insta-Clone/src/App.js
@@ -0,0 +1,21 @@
+/* 
+Start here and then work through the 
+PostsContainer components and the CommentSection Components. 
+Not all files need code added. 
+Look at each file to see where you need to pass props or add code 
+*/
+import React from "react";
+import "./App.css";
+// import the PostsPage and SearchBar and add them to the App
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      {/* Add imported components here to render them */}
+    </div>
+  );
+};
+
+export default App;
