0b93c6cc5a15876fca514ab9c7f0f44c137e2174
Reanna <reannalacasse@gmail.com>
2020-04-14 17:47:50 -0700

    mvp

--- u2/312/React-Components-Insta-Clone/src/components/CommentSection/CommentSectionContainer.js
+++ u2/312/React-Components-Insta-Clone/src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,11 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  const [comments] = useState(props.comments);
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map(com => <Comment key={com.username} comment={com} />)}
       <CommentInput />
     </div>
   );
